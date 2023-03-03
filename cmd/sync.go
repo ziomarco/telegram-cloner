2b0b6de0e2318dbf8ed2665faf8ab33e1f81d5dc
Marco Palmisano <djmarco.palmisano@gmail.com>
2023-03-03 01:00:57 +0100

    feat: Now it also forwards photo

--- cmd/sync.go
+++ cmd/sync.go
@@ -39,24 +39,63 @@ func sync(cmd *cobra.Command, args []string) {
 	}
 	chatList := http.GetChats(token)
 	parsedOrigin, parsedDestination := promptOriginDestination(chatList)
+	stringToReplace, replacement := promptSubstitution()
 	askForConfirm(parsedOrigin, parsedDestination)
 
 	log.Println(fmt.Sprintf("Using %s as origin and %s as destination", parsedOrigin, parsedDestination))
-	startSync(token, parsedOrigin, parsedDestination)
+	if len(stringToReplace) > 0 {
+		log.Println(fmt.Sprintf("Replacing \"%s\" with \"%s\"", stringToReplace, replacement))
+	}
+	startSync(token, parsedOrigin, parsedDestination, stringToReplace, replacement)
 }
 
-func startSync(token string, from string, to string) {
+func startSync(token string, from string, to string, stringToReplace string, replacement string) {
 	origin, _ := strconv.ParseInt(from, 10, 64)
 	destination, _ := strconv.ParseInt(to, 10, 64)
 	log.Println("Starting listening...")
 	telegram.ListenToMessages(token, func(m *tgbotapi.Message) {
 		log.Println("Handling message")
-		if m.From.ID != origin {
+		if m.From == nil && m.SenderChat == nil {
+			log.Println("Invalid update received!")
 			return
 		}
-		telegram.SendMessage(token, destination, m.Text)
+		if m.From != nil && m.From.ID != origin {
+			return
+		}
+		if m.SenderChat != nil && m.SenderChat.ID != origin {
+			return
+		}
+
+		var sender int64
+
+		if m.From != nil {
+			sender = m.From.ID
+		}
+		if m.SenderChat != nil {
+			sender = m.SenderChat.ID
+		}
+		if len(stringToReplace) == 0 {
+			telegram.CopyMessage(token, origin, destination, *m)
+			log.Println("Message forwarded")
+			return
+		}
+
+		parsedMessage := parseMessage(m, stringToReplace, replacement)
+		telegram.SendMessage(token, sender, destination, *parsedMessage)
 		log.Println("Message forwarded")
 	}, 0)
+}
+
+func parseMessage(message *tgbotapi.Message, stringToReplace string, replacement string) *tgbotapi.Message {
+	if len(message.Caption) > 0 {
+		message.Caption = strings.Replace(message.Caption, stringToReplace, replacement, -1)
+		return message
+	}
+	if len(message.Text) > 0 {
+		message.Text = strings.Replace(message.Text, stringToReplace, replacement, -1)
+		return message
+	}
+	return message
 }
 
 func promptOriginDestination(chatList []string) (string, string) {
@@ -78,6 +117,36 @@ func promptOriginDestination(chatList []string) (string, string) {
 	parsedDestination = strings.Trim(parsedDestination, "[")
 	parsedDestination = strings.Trim(parsedDestination, "]")
 	return parsedOrigin, parsedDestination
+}
+
+func promptSubstitution() (string, string) {
+	confirm := *prompt.Select(prompt.SelectPromptContent{
+		ErrorMsg: "Invalid input!",
+		Label:    fmt.Sprintf("Do you want to substitute some string in message forwarding?"),
+		Items:    []string{"yes", "no"},
+	})
+
+	if confirm != "yes" {
+		return "", ""
+	}
+
+	textToReplace := prompt.Input(prompt.InputPromptContent{
+		ErrorMsg:      "Invalid input!",
+		Label:         "Insert text to replace text",
+		ValidateFunc:  nil,
+		ValidateRegex: "",
+	})
+
+	replacement := prompt.Input(prompt.InputPromptContent{
+		ErrorMsg: "Invalid input!",
+		Label:    "Insert replacement",
+		ValidateFunc: func(s string) error {
+			return nil
+		},
+		ValidateRegex: "",
+	})
+
+	return textToReplace, replacement
 }
 
 func askForConfirm(origin string, destination string) {
--- telegram/telegram.go
+++ telegram/telegram.go
@@ -3,6 +3,7 @@ package telegram
 import (
 	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"log"
 )
 
 type MessageReceivedCallbackFunc = func(message *tgbotapi.Message)
@@ -14,18 +15,51 @@ func ListenToMessages(token string, cb MessageReceivedCallbackFunc, updateRange 
 	updates := client.GetUpdatesChan(config)
 
 	for update := range updates {
+		if &update == nil {
+			continue
+		}
 		if update.ChannelPost != nil {
 			cb(update.ChannelPost)
 		}
 		if update.Message != nil {
-			//log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 			cb(update.Message)
 		}
 	}
 }
 
-func SendMessage(token string, to int64, msgContent string) {
+func CopyMessage(token string, from int64, to int64, message tgbotapi.Message) {
 	client := initClient(token)
+	baseChat := tgbotapi.BaseChat{ChatID: to}
+	_, err := client.CopyMessage(tgbotapi.CopyMessageConfig{
+		BaseChat:            baseChat,
+		FromChatID:          from,
+		FromChannelUsername: "",
+		MessageID:           message.MessageID,
+		Caption:             message.Caption,
+	})
+	if err != nil {
+		log.Panicln(err)
+	}
+}
+
+func SendMessage(token string, from int64, to int64, message tgbotapi.Message) {
+	client := initClient(token)
+	var msgContent string
+	if len(message.Text) > 0 {
+		msgContent = message.Text
+	}
+	if len(message.Caption) > 0 {
+		msgContent = message.Caption
+	}
+	if len(message.Photo) > 0 {
+		photoId := message.Photo[len(message.Photo)-1].FileID
+		photoMessageContent := tgbotapi.NewPhoto(to, tgbotapi.FileID(photoId))
+		_, pErr := client.Send(photoMessageContent)
+		if pErr != nil {
+			log.Println("Error forwarding photo:")
+			log.Println(pErr)
+		}
+	}
 	msg := tgbotapi.NewMessage(to, msgContent)
 	client.Send(msg)
 }
