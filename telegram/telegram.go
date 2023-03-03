14fae586ea976612572819cac1a972b363c4c2fe
Marco Palmisano <djmarco.palmisano@gmail.com>
2023-03-03 01:09:31 +0100

    chore: Tidied up and made pipeline

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -1,17 +1,37 @@
 package telegram
 
 import (
-	"fmt"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"log"
 )
 
+var globalClient tgbotapi.BotAPI
+
 type MessageReceivedCallbackFunc = func(message *tgbotapi.Message)
 
+func getClient(token string) *tgbotapi.BotAPI {
+	if &globalClient != nil {
+		return &globalClient
+	}
+
+	bot, err := tgbotapi.NewBotAPIWithAPIEndpoint(token, "http://localhost:8081/user%s/%s")
+	bot.Debug = false
+	if err != nil {
+		log.Println("Can't init TGBotClient")
+		log.Fatalln(err)
+		return nil
+	}
+
+	globalClient = *bot
+	return &globalClient
+}
+
 func ListenToMessages(token string, cb MessageReceivedCallbackFunc, updateRange int) {
-	client := initClient(token)
+	client := getClient(token)
+
 	config := tgbotapi.NewUpdate(updateRange)
 	config.Timeout = 600
+
 	updates := client.GetUpdatesChan(config)
 
 	for update := range updates {
@@ -28,7 +48,8 @@ func ListenToMessages(token string, cb MessageReceivedCallbackFunc, updateRange 
 }
 
 func CopyMessage(token string, from int64, to int64, message tgbotapi.Message) {
-	client := initClient(token)
+	client := getClient(token)
+
 	baseChat := tgbotapi.BaseChat{ChatID: to}
 	_, err := client.CopyMessage(tgbotapi.CopyMessageConfig{
 		BaseChat:            baseChat,
@@ -37,20 +58,25 @@ func CopyMessage(token string, from int64, to int64, message tgbotapi.Message) {
 		MessageID:           message.MessageID,
 		Caption:             message.Caption,
 	})
+
 	if err != nil {
 		log.Panicln(err)
 	}
 }
 
 func SendMessage(token string, from int64, to int64, message tgbotapi.Message) {
-	client := initClient(token)
+	client := getClient(token)
+
 	var msgContent string
+
 	if len(message.Text) > 0 {
 		msgContent = message.Text
 	}
+
 	if len(message.Caption) > 0 {
 		msgContent = message.Caption
 	}
+
 	if len(message.Photo) > 0 {
 		photoId := message.Photo[len(message.Photo)-1].FileID
 		photoMessageContent := tgbotapi.NewPhoto(to, tgbotapi.FileID(photoId))
@@ -60,21 +86,7 @@ func SendMessage(token string, from int64, to int64, message tgbotapi.Message) {
 			log.Println(pErr)
 		}
 	}
+
 	msg := tgbotapi.NewMessage(to, msgContent)
 	client.Send(msg)
-}
-
-func initClient(token string) *tgbotapi.BotAPI {
-	bot, err := tgbotapi.NewBotAPIWithAPIEndpoint(token, "http://localhost:8081/user%s/%s")
-	bot.Debug = false
-	if err != nil {
-		fmt.Println("Can't init TGBotClient")
-		fmt.Println(err)
-		return nil
-	}
-	return bot
-}
-
-func GetClient(token string) *tgbotapi.BotAPI {
-	return initClient(token)
 }
